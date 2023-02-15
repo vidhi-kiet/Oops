c3793659cbc6817fe32d27f98a172b7d75c8ce28
vidhi <78697483+vidhi-kiet@users.noreply.github.com>
2023-02-16 00:22:01 +0530

    Add files via upload

---
+++ staticdatamember.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+class demo
+{
+    int x,y;
+    static int z;
+    public:
+    
+    void getdata()
+    {
+       cout<<"\nEnter no.: ";
+       cin>>x>>y;
+       z++;
+        
+    }
+    
+    void putdata()
+    {
+        cout<<"\nx = "<<x<<"\ny = "<<y;
+        cout<<"\nstatic z = "<<z;
+    }
+};
+
+int demo ::z;
+ 
+
+
+int main()
+{
+ demo aa,bb;
+ aa.getdata();
+ bb.getdata();
+ aa.putdata();
+ bb.putdata();
+    return 0;
+}
